d4b78cb69a160978e69e4a1741f3c272634e0212
HenriMalahieude <henrimalahieude@gmail.com>
2024-04-28 02:30:45 -0700

    sktn for update_articles

--- CS110/lab4/main.js
+++ CS110/lab4/main.js
@@ -1,3 +1,5 @@
+let dbnc = Date.now();
+
 function clear_articles() {
 	let article_container = document.getElementsByClassName("article-container")[0];
 	
@@ -23,4 +25,37 @@ function add_article(title = "?) Dummy Article", date = "YYYY-MM-DD", img_src, d
 
 	let article_container = document.getElementsByClassName("article-container")[0];
 	article_container.innerHTML += individual_article;
+}
+
+var getting = false; //debounce
+async function update_articles() {
+	if (getting) return;
+	getting = true;
+
+	clear_articles();
+
+	let dif = (Date.now() - dbnc) / 1000;
+	console.log("So this is the dif " + dif);
+	if (dif < 15) await new Promise(r => setTimeout(r, (15 - dif)*1000));
+	dbnc = Date.now();
+
+	//get settings
+	let srtbies = document.getElementsByName("sortbyer");
+	let srtby = "Emailed";
+	if (srtbies[0].checked) srtby = "Viewed";
+	if (srtbies[1].checked) srtby = "Shared";
+
+	let tframes = document.getElementsByName("timeframe");
+	let tfrme = "Month";
+	if (tframes[0].checked) tfrme = "Day"
+	if (tframes[1].checked) tfrme = "Week"
+
+	const url = "";
+	fetch(url).then(res => res.json).then(data => {
+		console.log(data)
+		getting = false;
+	}).catch(err => {
+		console.log(err)
+		getting = false;
+	})//*/
 }
